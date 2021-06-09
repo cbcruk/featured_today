a40dd958265c27c090ba797dc61446cc8c7f1027
cbcruk <cbcruk@gmail.com>
2021-06-10 07:07:38 +0900

    :heavy_minus_sign: update: writeJSON,  readJSONFromURL 제거

--- postprocess.js
+++ postprocess.js
@@ -1,8 +1,4 @@
 import { format } from 'https://deno.land/std@0.97.0/datetime/mod.ts'
-import {
-  writeJSON,
-  readJSONFromURL,
-} from 'https://deno.land/x/flat@0.0.10/mod.ts'
 
 const API_URL = 'https://sensortower.com/api/ios/featured/today/stories'
 const COUNTRIES = [
@@ -23,12 +19,13 @@ const date = format(new Date(), 'yyyy-MM-dd')
 
 for (const country of COUNTRIES) {
   try {
-    const data = await readJSONFromURL(
+    const response = await fetch(
       `${API_URL}?country=${country}&start_date=${date}}`
     )
+    const data = await response.json()
     const newFilename = `./raw_data/${country}/${date}.json`
 
-    await writeJSON(newFilename, data[0])
+    await Deno.writeTextFile(newFilename, JSON.stringify(data[0]))
   } catch (error) {
     console.error(error)
   }
