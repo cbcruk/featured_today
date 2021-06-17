b6546a6b2027a203eee0efe7cd59aad6197d8441
cbcruk <cbcruk@gmail.com>
2021-06-17 15:48:21 +0900

    :lipstick: update: 다크모드 추가

--- components/List.js
+++ components/List.js
@@ -4,9 +4,11 @@ import Grid from './Grid'
 function List({ label, title, apps, url }) {
   return (
     <Box>
-      <div className="bg-gray-200">
+      <div className="bg-gray-200 dark:bg-gray-800">
         <div className="py-8 px-5 font-bold">
-          <div className="text-md text-gray-600">{label}</div>
+          <div className="text-md text-gray-600 dark:text-gray-300">
+            {label}
+          </div>
           <a href={url} target="_blank" rel="noreferrer" className="text-3xl">
             {title}
           </a>
--- pages/_document.js
+++ pages/_document.js
@@ -3,14 +3,14 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class CustomDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="ko">
         <Head>
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap"
           />
         </Head>
-        <body>
+        <body className="dark:bg-gray-900 dark:text-gray-300">
           <Main />
           <NextScript />
         </body>
